69138c4955ff5fb116d47e9715ecce7342f58bf6
LeeYeongin <youngin5757@gmail.com>
2024-02-26 15:11:03 +0900

    2, 3장 연습문제

---
+++ practice_ch02.py
@@ -0,0 +1,12 @@
+# 2-3
+name = 'Lee yeongin'
+message = f"안녕하세요. {name.title()}, 오늘은 파이썬을 배워 보는 게 어떨까요?"
+print(message)
+print()
+
+# 2-4
+print(f"대문자: {name.upper()}")
+print(f"소문자: {name.lower()}")
+print(f"첫글자만: {name.title()}")
+
+#
