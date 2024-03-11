deb7faf2d5b42084348a51dbdd048d3987d101cd
LeeYeongin <youngin5757@gmail.com>
2024-03-11 12:24:38 +0900

    우주비행선 키보드로 이동

--- ch12/space.py
+++ ch12/space.py
@@ -9,19 +9,31 @@ clock = pygame.time.Clock()
 image = pygame.image.load('./images/ship.bmp') # 이미지 로드
 # 직사각형 각도 좌표를 저장하는 파이게임 객체
 # rect = pygame.Rect((1280/2, 720/2), (200, 200)) # Rect((left, top), (width, height))
-image_rect = image.get_rect()
+screen_rect = screen.get_rect()
+ship_rect = image.get_rect()
 # 직접 지정해줄 수 있음
 # image_rect.left = 1280/2 
 # image_rect.top = 600
-image_rect.midbottom = screen.get_rect().midbottom
+ship_rect.midbottom = screen.get_rect().midbottom
 
 while True:
     # Process player inputs.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            # raise SystemExit
-            sys.exit()
+            sys.exit() # raise SystemExit와 동일
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ship_rect.left -= 5
+            elif event.key == pygame.K_RIGHT:
+                ship_rect.right += 5
+            elif event.key == pygame.K_UP:
+                ship_rect.top -= 5
+            elif event.key == pygame.K_DOWN:
+                # if ship_rect.bottom <= screen_rect.bottom:
+                if (ship_rect.top + ship_rect.height) <= screen_rect.height: 
+                    ship_rect.bottom += 5
+            
 
     # Do logical updates here. 
     ### 1. 키보드 마우스 이벤트
@@ -31,8 +43,8 @@ while True:
 
     # Render the graphics here.
     # ...
-    # screen.blit(image, image_rect)
-    screen.blit(image, image_rect)
+    # screen.blit(image, rect)
+    screen.blit(image, ship_rect)
 
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)         # wait until next frame (at 60 FPS)
